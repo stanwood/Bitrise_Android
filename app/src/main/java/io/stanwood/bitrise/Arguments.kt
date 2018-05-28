d2b2294fc4f2b975fc5371bfd3f89fe725a9b3f1
Milosz Moczkowski <milosz.moczkowski@gmail.com>
2018-05-28 12:11:27 +0200

    Add navigation

---
+++ app/src/main/java/io/stanwood/bitrise/Arguments.kt
@@ -0,0 +1,14 @@
+package io.stanwood.bitrise
+
+
+object Arguments {
+    const val ACTIVITY = "activity"
+    const val BUILD = "build"
+    const val APP = "arg_app"
+    const val FRAGMENT_MANAGER = "fragment_manager"
+    const val BRANCH = "branch"
+    const val WORKFLOW = "workflow"
+    const val FAVORITE_APPS = "fav_apps"
+    const val MESSAGE = "arg_message"
+    const val TOKEN = "arg_token"
+}
